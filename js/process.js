b48d9d8471e274c73abe81131dbbd5740dcf4413
Bùi Văn Xứng UIT <xungbv.uit@gmail.com>
2019-03-01 18:13:33 +0700

    update database

--- js/process.js
+++ js/process.js
@@ -1,7 +1,7 @@
 var socket;
 var infor_data,infor_data_update, new_admin,new_date,new_email,new_name_product,new_seri_number,new_token;
 var etable = "";
-var update_last_token,update_seri_number,update_email, update_admin, update_new_token = "";
+var update_last_token,update_seri_number,update_email, update_admin, update_new_token;
        $(document).ready(function() {
         socket = io.connect('http://wirelesstech.online:5060',{
           reconnection: true,
@@ -38,6 +38,8 @@ var update_last_token,update_seri_number,update_email, update_admin, update_new_
         update_email = $("input[name='update_email']").val();
         update_new_token = $("input[name='update_new_token']").val();
         infor_data_update = update_seri_number+"&"+update_last_token+"&"+update_new_token+"&"+update_email+"&"+update_admin;
+        console.log(update_last_token + update_last_token);
+        
     });
     function onSubmitNewdata(){
       socket.emit("new_data",infor_data);
