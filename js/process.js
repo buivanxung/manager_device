7ab03e6dc9deaee6459dcb4aed7f30d46b023537
Bùi Văn Xứng UIT <xungbv.uit@gmail.com>
2019-03-01 11:29:15 +0700

    update database

--- js/process.js
+++ js/process.js
@@ -25,13 +25,10 @@ var infor_data,infor_data_update, new_admin,new_date,new_email,new_name_product,
         new_token = $("input[name='new_token']");
         new_seri_number = $("input[name='new_seri_number']");
         new_date = $("input[name='new_date']");
-        function displayVals() {
-          var new_name_product = $( "#new_name_product" ).val();
-          var new_admin = $( "#new_admin" ).val();
-        }
-        $( "select" ).change( displayVals );
-        displayVals();
+        var new_name_product = $( "#new_name_product" ).val();
+        var new_admin = $( "#new_admin" ).val();
         infor_data = new_seri_number+"&"+ new_token + "&"+ new_email + "&" + new_name_product + "&" + new_admin+ "&" + new_date;
+        console.log(infor_data);
     });
     function onSubmitNewdata(){
       console.log(infor_data);
